a97a709333474ad8b5c5119a42de98fb594629b4
mikaelec <mikaelec@gmail.com>
2014-11-25 22:48:40 +0100

    Added message region, removed unused id

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var App = new Backbone.Marionette.Application();
-App.addRegions({ container: "#container" });
+App.addRegions({ container: "#container", message: "#message" });
 App.on("start", function (options) {
 	if (Backbone.history) {
   	Backbone.history.start();
