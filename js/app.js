eadd5975a881fce200fdb14b3eac8367b99d85b1
mikaelec <mikaelec@gmail.com>
2014-11-26 12:01:42 +0100

    Added alert view

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var App = new Backbone.Marionette.Application();
-App.addRegions({ container: "#container", message: "#message" });
+App.addRegions({ container: "#container", alert: "#alert" });
 App.on("start", function (options) {
 	if (Backbone.history) {
   	Backbone.history.start();
---
+++ js/modules/shared/views/Alert.js
@@ -0,0 +1,30 @@
+App.module("Shared.Views", function(Views, App, Backbone, Marionette, $, _) {
+	Views.Alert = Backbone.Marionette.ItemView.extend({
+		template: "#alert-view",
+		message: "No message.",
+		alertClass: "alert-warning",
+
+		ui: {
+			alertMain: "div.alert"
+		},
+
+		initialize: function(options) {
+			var options = options || {};
+
+			if (options.message)
+				this.message = options.message;
+
+			if (options.alertClass && validAlertClass(options.alertClass))
+				this.alertClass = options.alertClass;
+		},
+
+		serializeData: function() {
+			return { message: this.message, alertClass: this.alertClass };
+		}
+	});
+
+	var validAlertClass = function(name) {
+		var classes = ["alert-success", "alert-info", "alert-warning", "alert-danger"];
+		return classes.indexOf(name) > -1;
+	};
+});
