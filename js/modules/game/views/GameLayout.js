15359267010f1c416f2654ce863f92108b224586
mikaelec <mikaelec@gmail.com>
2014-11-30 12:26:17 +0100

    Getting and showing players should now work properly

--- js/modules/game/views/GameLayout.js
+++ js/modules/game/views/GameLayout.js
@@ -22,16 +22,34 @@ App.module("Game.Views", function(Views, App, Backbone, Marionette, $, _) {
 
 			this.titleModel = new App.Game.Models.Title();
 			this.titleModel.on("change:title", function(model, change) { self.updateTitle(change) });
-      this.titleInterval = setInterval(function() { self.titleModel.fetch() }, 500);
 
       this.descriptionModel = new App.Game.Models.Description();
       this.descriptionModel.on("change:description", function(model, change) { self.updateDescription(change)});
+
+      this.playerCollection = new App.Game.Models.PlayerCollection();
+      this.colView = new Backbone.Marionette.CollectionView({ childView: Views.Player, collection: this.playerCollection });
+      this.playerCollection.on("add", function(model, collection, xhr) { self.updatePlayers()});
+      this.playerCollection.on("remove", function(model, collection, xhr) { self.updatePlayers()});
+      this.playerCollection.on("change", function(model, collection, xhr) { self.updatePlayers()});
+		},
+
+		onShow: function() {
+			var self = this;
+
+			self.titleModel.fetch()
+      this.titleInterval = setInterval(function() { self.titleModel.fetch() }, 500);
+
+      self.descriptionModel.fetch()
       this.descriptionInterval = setInterval(function() { self.descriptionModel.fetch() }, 500);
 
-      this.colView = new Backbone.Marionette.CollectionView({ childView: Views.Player, collection: this.playerCollection });
-      this.playerCollection = new App.Game.Models.PlayerCollection();
-      this.playerCollection.on("add", function() { self.updatePlayers() });
-      this.playerCollection.fetch();
+      self.playerCollection.fetch();
+      this.playerInterval = setInterval(function() { self.playerCollection.fetch() }, 500);
+		},
+
+		onDestroy: function() {
+			clearInterval(this.titleInterval);
+			clearInterval(this.descriptionInterval);
+			clearInterval(this.playerInterval);
 		},
 
 		updateTitle: function(title) {
