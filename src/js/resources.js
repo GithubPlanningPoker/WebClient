c2eccc9f0a6bb8d807fb2497d3e83e4afec56603
MikaelEC <mikaelec@gmail.com>
2015-03-17 20:54:59 +0100

    Set resource base route to api/

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,25 +2,25 @@
   angular.module('ghpp')
 
     .factory('Games', ['$resource', function ($resource) {
-      return $resource('../../ghpp/game/:gameId');
+      return $resource('api/game/:gameId');
     }])
 
     .factory('Users', ['$resource', function ($resource) {
-      return $resource('../../ghpp/game/:gameId/user/:user', null, {
+      return $resource('api/game/:gameId/user/:user', null, {
         query: { method: 'GET' },
         update: { method: 'PUT' }
       });
     }])
 
     .factory('GameTitle', ['$resource', function ($resource) {
-      return $resource('../../ghpp/game/:gameId/title', null, {
+      return $resource('api/game/:gameId/title', null, {
         query: { method: 'GET' },
         update: { method: 'PUT' }
       });
     }])
 
     .factory('GameDescription', ['$resource', function ($resource) {
-      return $resource('../../ghpp/game/:gameId/description', null, {
+      return $resource('api/game/:gameId/description', null, {
         query: { method: 'GET' },
         update: { method: 'PUT' }
       });
